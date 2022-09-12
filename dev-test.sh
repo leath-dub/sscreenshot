b58a8c14e7d288469af5a5152aa4e6ddaffae5e0
leath-dub <7788920+leath-dub@users.noreply.github.com>
2022-09-12 17:24:31 +0100

    make dev-test.sh posix compliant because 3 months ago I thought that zsh was hip, I was but a mere script kiddy, borderline apple user with a fancy zsh prompt ++ plugins, now yash shell ftw !

--- dev-test.sh
+++ dev-test.sh
@@ -1,17 +1,58 @@
-#!/usr/bin/zsh
+#!/bin/sh
 
 prev=$SS_DIR
-export SS_DIR=$(pwd)
+export SS_DIR=$PWD
+
+is_png()
+{
+  tst=$(hexdump $1 | head -1 | awk '{print $2, $3}')
+  [ "$tst" = "5089 474e" ] && return 0
+  return 1
+}
 
 test()
 {
-  which sss && \
-    sss && [[ $(hexdump ./screenshot.png | head -1 | awk '{print $2,$3}') == "5089 474e" ]] \
-    && sss -n && [[ $(hexdump ./screenshot.png | head -1 | awk '{print $2,$3}') == "5089 474e" ]] \
-    && sss -f && [[ $(hexdump ./fullscreen.png | head -1 | awk '{print $2,$3}') == "5089 474e" ]] \
-    && sss -n test.png && [[ $(hexdump ./test.png | head -1 | awk '{print $2,$3}') == "5089 474e" ]] \
-    && sss -f test.png && [[ $(hexdump ./test.png | head -1 | awk '{print $2,$3}') == "5089 474e" ]] \
-    && cleanup
+  type sss && {
+
+    # test 1
+    sss && {
+      is_png ./screenshot.png || {
+        echo "failed at test 1"
+        exit 1
+      }
+    }
+
+    # test 2
+    sss -n && {
+      is_png ./screenshot.png || {
+        echo "failed at test 2"
+        exit 1
+      }
+    }
+
+    sss -f && {
+      is_png ./screenshot.png || {
+        echo "failed at test 3"
+        exit 1
+      }
+    }
+
+    sss -n test.png && {
+      is_png ./test.png || {
+        echo "failed at test 4"
+        exit 1
+      }
+    }
+
+    sss -f test.png && {
+      is_png ./test.png || {
+        echo "failed at test 5"
+        exit 1
+      }
+    }
+
+    cleanup
+  }
 }
 
 cleanup()
