a3a3fcf5a54ec9b227bded0f1803e520336029a5
leath-dub <7788920+leath-dub@users.noreply.github.com>
2022-09-12 16:06:19 +0100

    changed color to green to it is more visible

--- config.h
+++ config.h
@@ -1,3 +1,3 @@
 #define LINE_WIDTH 4 /* width of drawn box lines */
-#define LINE_COLOR 0xEBDBB2 /* color of drawn box lines */
+#define LINE_COLOR 0xA3BE8C /* color of drawn box lines */
 #define CURSOR "cross" /* refer to your cursor theme /usr/share/icons */
